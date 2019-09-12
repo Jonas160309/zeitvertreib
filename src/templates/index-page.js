a84d1bcd36dd34f0de6ef7834664a02d63134a7e
Jonas160309 <55091481+Jonas160309@users.noreply.github.com>
2019-09-12 17:05:44 +0200

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -12,7 +12,6 @@ export const IndexPageTemplate = ({
   heading,
   subheading,
   mainpitch,
-  description,
   intro,
 }) => (
   <div>
@@ -74,16 +73,12 @@ export const IndexPageTemplate = ({
                   <div className="tile">
                     <h1 className="title">{mainpitch.title}</h1>
                   </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
                 </div>
                 <div className="columns">
                   <div className="column is-12">
                     <h3 className="has-text-weight-semibold is-size-2">
                       {heading}
                     </h3>
-                    <p>{description}</p>
                   </div>
                 </div>
                 <Features gridItems={intro.blurbs} />
@@ -120,7 +115,6 @@ IndexPageTemplate.propTypes = {
   heading: PropTypes.string,
   subheading: PropTypes.string,
   mainpitch: PropTypes.object,
-  description: PropTypes.string,
   intro: PropTypes.shape({
     blurbs: PropTypes.array,
   }),
@@ -137,7 +131,6 @@ const IndexPage = ({ data }) => {
         heading={frontmatter.heading}
         subheading={frontmatter.subheading}
         mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
         intro={frontmatter.intro}
       />
     </Layout>
@@ -170,9 +163,7 @@ export const pageQuery = graphql`
         subheading
         mainpitch {
           title
-          description
         }
-        description
         intro {
           blurbs {
             image {
@@ -185,7 +176,6 @@ export const pageQuery = graphql`
             text
           }
           heading
-          description
         }
       }
     }
