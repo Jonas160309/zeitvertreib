b8feeef75507df966d9837bc57b869f9e752d183
Jonas160309 <55091481+Jonas160309@users.noreply.github.com>
2019-09-12 15:06:21 +0200

    Update about-page.js

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -13,7 +13,16 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light"
+                style={{
+                  boxShadow:
+                    'rgb(192, 192, 192) 0.5rem 0px 0px, rgb(192, 192, 192) -0.5rem 0px 0px',
+                  backgroundColor: 'rgb(192, 192, 192)',
+                  color: 'white',
+                  lineHeight: '1',
+                  padding: '0.25em',
+                }}
+              >
                 {title}
               </h2>
               <PageContent className="content" content={content} />
