2493c1b93b07da565ac1149fd289797125b3eeae
Jonas160309 <55091481+Jonas160309@users.noreply.github.com>
2019-10-20 16:35:34 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Zeitvertreib',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Verein für qualitativ hochwertige Ferienbetreuung und Freizeitgestaltung für Kinder in Dornbirn',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
