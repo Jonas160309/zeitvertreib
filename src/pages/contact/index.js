9c18ea941b5027ab8e6e19fb0d3c7618eff0597e
Jonas160309 <55091481+Jonas160309@users.noreply.github.com>
2019-12-13 15:37:46 +0100

    Update index.js

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -39,6 +39,73 @@ export default class Index extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
+              <h1>Schreiben Sie uns</h1>
+              <form
+                name="contact"
+                method="post"
+                action="/contact/thanks/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={this.handleSubmit}
+              >
+                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                <input type="hidden" name="form-name" value="contact" />
+                <div hidden>
+                  <label>
+                    Don’t fill this out:{' '}
+                    <input name="bot-field" onChange={this.handleChange} />
+                  </label>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={'name'}>
+                    Name
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type={'text'}
+                      name={'name'}
+                      onChange={this.handleChange}
+                      id={'name'}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={'email'}>
+                    Email-Adresse
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type={'email'}
+                      name={'email'}
+                      onChange={this.handleChange}
+                      id={'email'}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={'message'}>
+                    Nachricht
+                  </label>
+                  <div className="control">
+                    <textarea
+                      className="textarea"
+                      name={'message'}
+                      onChange={this.handleChange}
+                      id={'message'}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <button className="button is-link" type="submit">
+                    Senden
+                  </button>
+                </div>
+              </form>
               <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeNjCuVaMed6J5NUjB497J_TYx45L1Zoci-g7WgaUvGa8RMkw/viewform?embedded=true" width="640" height="790" frameborder="0" marginheight="0" marginwidth="0">Wird geladen…</iframe>
             </div>
           </div>
